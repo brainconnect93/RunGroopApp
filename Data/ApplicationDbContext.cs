2363cce9f88210e3e99cb69fdef137ef7ae4b343
Afolabi Akorede <brainconnect0@gmail.com>
2024-05-06 21:48:08 +0300

    Install all the necessary Entity frameworks and seed data to the database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroopApp.Models;
+
+namespace RunGroopApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,11 @@
-﻿ namespace RunGroopApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopApp.Models
 {
     public class User
     {
+        [Key]
+        public string Id { get; set; }
         public int? Pace { get; set; }
         public int? Mileage { get; set; }
         public Address? Address { get; set; }
