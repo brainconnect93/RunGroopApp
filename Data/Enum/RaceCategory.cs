182447b4311c1563ad606bd0450bf51779412e22
Afolabi Akorede <brainconnect0@gmail.com>
2024-05-06 17:46:06 +0300

    Work with the model (Address, Club, Race, User) with their  DataCategory

---
+++ Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroopApp.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMarathon
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿ namespace RunGroopApp.Models
+{
+    public class User
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}
