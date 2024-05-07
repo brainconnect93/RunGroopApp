54c109c6d58eef0911062d9c88b736a1b8b77550
Afolabi Akorede <brainconnect0@gmail.com>
2024-05-07 14:35:11 +0100

    complete the view and controller for the Club page

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopApp.Data;
+using RunGroopApp.Models;
 
 namespace RunGroopApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
