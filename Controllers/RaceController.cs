4874727424fba3a22b0d93d3ef70a48c52398845
Afolabi Akorede <brainconnect0@gmail.com>
2024-05-07 14:48:42 +0100

    Complete the view for the Home, Club and Race pages

--- Controllers/RaceController.cs
+++ Controllers/RaceController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopApp.Data;
+using RunGroopApp.Models;
 
 namespace RunGroopApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
