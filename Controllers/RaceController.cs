ec60c41b73668949592c33c0fae312a7bfd1cd45
Afolabi Akorede <brainconnect0@gmail.com>
2024-05-07 12:34:48 +0100

    Complete the controller of the rungroop app

---
+++ Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroopApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
